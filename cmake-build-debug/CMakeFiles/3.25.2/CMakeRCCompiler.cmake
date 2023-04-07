2e3a9591923dfe65e937e0b5c2cd86d5776fbea1
D-Gaspa <diego.gaspariseo@udlap.mx>
2023-04-06 20:03:00 -0600

    Added reset button and its functionality

--- cmake-build-debug/CMakeFiles/3.25.2/CMakeRCCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.25.2/CMakeRCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_RC_COMPILER "C:/Users/diego/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.174/bin/mingw/bin/windres.exe")
+set(CMAKE_RC_COMPILER "C:/Users/diego/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/bin/windres.exe")
 set(CMAKE_RC_COMPILER_ARG1 "")
 set(CMAKE_RC_COMPILER_LOADED 1)
 set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/diego/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.174/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/diego/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
